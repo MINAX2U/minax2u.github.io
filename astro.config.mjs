ab7593545c04fcdcad2cc430455d9337ed6ae1e2
賴奕辰 <ethanlai588@proton.me>
2025-06-09 23:57:08 +0800

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "minax2u.github.io",
+  site: "https://minax2u.github.io/",
   base: "/",
   trailingSlash: "always",
   integrations: [
--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ import type {
 import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
-  title: 'xiunG',
+  title: 'MINAX',
   subtitle: 'Blog',
   lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
